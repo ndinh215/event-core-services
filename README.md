f8cec47212957b5cafade0d94b66b99878994c32
Nghia.Dinh <blackjackptit@gmail.com>
2023-01-12 11:00:54 +0700

    Initialize the project.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# event-core-services
+# Event core services
+This projects contain core services used by functions or APIs.
---
+++ src/main/java/com/company/applications/FunctionApplication.java
@@ -0,0 +1,15 @@
+package com.company.applications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class FunctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionApplication.class, args);
+    }
+}
